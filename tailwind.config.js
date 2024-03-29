8446e7fc37f7441a195f0e7b1a5b24cc3ec3fe7f
Letícia Lima <leticiaakiralele2018@gmail.com>
2024-03-28 23:15:27 -0300

    quarto commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
     'offwhitebg': '#f2f2f2'
    },
    fontFamily: {
-    'shoulders': '"Big Shouders Display"',
+    'shoulders': '"Big Shoulders Display"',
     'lexend': '"Lexend Deca"'
    }
   },
