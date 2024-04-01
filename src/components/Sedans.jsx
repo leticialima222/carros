f41da64fdbdcf6723eb3b7d93a5132a9c8b434a9
Letícia Lima <leticiaakiralele2018@gmail.com>
2024-03-31 21:37:36 -0300

    criação de componentes

---
+++ src/components/Sedans.jsx
@@ -0,0 +1,14 @@
+import iconSedans from "../assets/icon-sedans.svg"
+
+import React from 'react'
+
+export default function Sedans() {
+    return (
+        <div className="bg-orange p-12 max-leg:rounded-t-lg lg:rounded-l-lg">
+          <img src={iconSedans} alt="icone dos carros sedan" className="mb-7" />
+          <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Sedans</h2>
+          <p className="font-lexend mb-7 text-offwhitebg lg:mb-20">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg" href="https://www.webmotors.com.br/" target="_blanck">Ver mais</a>
+        </div>
+    )
+}
